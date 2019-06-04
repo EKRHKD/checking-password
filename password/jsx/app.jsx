1b81361002ee9c2dbef43279d9c2f24c00eb0862
zxcsfs <ekrhkd5434@naver.com>
2019-06-05 00:37:02 +0900

    checing password

---
+++ password/jsx/app.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const Password = require('./password.jsx')
+
+ReactDOM.render(<Password
+    upperCase={true}
+    lowerCase={true}
+    special={true}
+    number={true}
+    over6={true}/>,
+  document.getElementById('password'))
+  console.log("Painless JavaScript password!")
